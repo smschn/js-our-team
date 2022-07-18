d0c7b53ad72c5434ab46c7b1c4da09708ceaa8f9
Luca Somaschini <somas99@gmail.com>
2022-07-18 18:36:26 +0200

    Bonus_2: implemented js for new graphic

--- Bonus_2/js/main.js
+++ Bonus_2/js/main.js
@@ -41,11 +41,27 @@ const arrayTeam = [
 
 // stampo su html i valori di ogni singola chiave, creando un nuovo <div> e riempiendolo ad ogni giro del ciclo
 for (let y = 0; y < arrayTeam.length; y++) {
-    const newDiv = document.createElement('div'); // creo <div>
+    
     const containerDom = document.getElementById('container'); // richiamo il container
+    const newDivCard = document.createElement('div'); // creo un nuovo <div> (sarà la card che contiene l'img e il testo)
+    newDivCard.classList.add('card'); // aggiungo la classe al <div> appena creato
+    containerDom.append(newDivCard); // aggiungo il <div> card al container
+
+    newDivCard.innerHTML = `<img src='${arrayTeam[y]['profilePicture']}' alt=Profile_pic />`; // inserisco l'img in card
+
+    const newDivName = document.createElement('p');
+    newDivCard.append(newDivName);
+    newDivName.innerHTML += arrayTeam[y]['name'];
+
+    const newDivRole = document.createElement('p');
+    newDivCard.append(newDivRole);
+    newDivRole.innerHTML += arrayTeam[y].role;
+
+
+    /* const newDiv = document.createElement('div'); // creo <div>
     containerDom.append(newDiv); // inserisco il <div> nel container
     newDiv.innerHTML = (y+1) + ' --- '; // inserisco uno alla volta i singoli valori delle chiavi
     newDiv.innerHTML += arrayTeam[y]['name'] + ' --- ';
     newDiv.innerHTML += arrayTeam[y].role + ' --- ';
-    newDiv.innerHTML += `<img src='${arrayTeam[y]['profilePicture']}' alt=Profile_pic />`; // oppure posso mettere direttamente nella chiave 'profilePic' l'indirizzo ad ogni singola immagine dentro al tag <img>
+    newDiv.innerHTML += `<img src='${arrayTeam[y]['profilePicture']}' alt=Profile_pic />`; // oppure posso mettere direttamente nella chiave 'profilePic' l'indirizzo ad ogni singola immagine dentro al tag <img> */
 }

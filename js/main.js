31fccd88f1b078e66bf730bf29fc579ace0aff8a
Luca Somaschini <somas99@gmail.com>
2022-07-18 16:25:54 +0200

    Milestone 2: completed

--- js/main.js
+++ js/main.js
@@ -46,3 +46,14 @@ for (let x = 0; x < arrayTeam.length; x++) {
     console.log(arrayTeam[x]['profilePicture']);
     console.log(''); // creo spazio per rendere risultati più leggibili nella console
 }
+
+// stampo su html i valori di ogni singola chiave, creando un nuovo <div> e riempiendolo ad ogni giro del ciclo
+for (let y = 0; y < arrayTeam.length; y++) {
+    const newDiv = document.createElement('div'); // creo <div>
+    const containerDom = document.getElementById('container'); // richiamo il container
+    containerDom.append(newDiv); // inserisco il <div> nel container
+    newDiv.innerHTML = (y+1) + ' --- '; // inserisco uno alla volta i vari valori
+    newDiv.innerHTML += arrayTeam[y]['name'] + ' --- ';
+    newDiv.innerHTML += arrayTeam[y].role + ' --- ';
+    newDiv.innerHTML += arrayTeam[y]['profilePicture'];
+}

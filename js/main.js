6ea79f5e1920b7916a204bc4661846128e2a65b5
Luca Somaschini <somas99@gmail.com>
2022-07-18 16:02:39 +0200

    Milestone 0: completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
-console.log('test');
+const arrayTeam = [
+
+    {
+        'name': 'Wayne Barnett',
+        'role': 'Founder & CEO',
+        'profilePicture': 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'profilePicture': 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'profilePicture': 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'profilePicture': 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'profilePicture': 'scott-estrada-developer.jpg'
+    },
+
+    {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'profilePicture': 'barbara-ramos-graphic-designer.jpg'
+    }
+
+]

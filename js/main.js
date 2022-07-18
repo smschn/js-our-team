e5dd946e36017ae3888f21e8ffef5bed224c50d5
Luca Somaschini <somas99@gmail.com>
2022-07-18 16:10:35 +0200

    Milestone 1: completed

--- js/main.js
+++ js/main.js
@@ -1,39 +1,48 @@
+// creo array con oggetti nella lista passataci
 const arrayTeam = [
 
     {
         'name': 'Wayne Barnett',
-        'role': 'Founder & CEO',
+        role: 'Founder & CEO',
         'profilePicture': 'wayne-barnett-founder-ceo.jpg'
     },
 
     {
         'name': 'Angela Caroll',
-        'role': 'Chief Editor',
+        role: 'Chief Editor',
         'profilePicture': 'wayne-barnett-founder-ceo.jpg'
     },
 
     {
         'name': 'Walter Gordon',
-        'role': 'Office Manager',
+        role: 'Office Manager',
         'profilePicture': 'walter-gordon-office-manager.jpg'
     },
 
     {
         'name': 'Angela Lopez',
-        'role': 'Social Media Manager',
+        role: 'Social Media Manager',
         'profilePicture': 'angela-lopez-social-media-manager.jpg'
     },
 
     {
         'name': 'Scott Estrada',
-        'role': 'Developer',
+        role: 'Developer',
         'profilePicture': 'scott-estrada-developer.jpg'
     },
 
     {
         'name': 'Barbara Ramos',
-        'role': 'Graphic Designer',
+        role: 'Graphic Designer',
         'profilePicture': 'barbara-ramos-graphic-designer.jpg'
     }
 
 ]
+
+// stampo su console i valori di 'name', 'role', 'profilePicture' per ogni oggetto
+for (let x = 0; x < arrayTeam.length; x++) {
+    console.log(arrayTeam[x]['name']);
+    console.log(arrayTeam[x]['role']);
+    console.log(arrayTeam[x]['profilePicture']);
+    console.log(''); // creo spazio per rendere risultati più leggibili nella console
+}

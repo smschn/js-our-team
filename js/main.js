f42702bed05d3bad205663976fda5ea10509cc3c
Luca Somaschini <somas99@gmail.com>
2022-07-18 16:31:53 +0200

    Improved comments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// creo array con oggetti nella lista passataci
+// creo un array con oggetti nella lista passataci
 const arrayTeam = [
 
     {
@@ -39,12 +39,12 @@ const arrayTeam = [
 
 ]
 
-// stampo su console i valori di 'name', 'role', 'profilePicture' per ogni oggetto
+// per ogni singolo oggetto, stampo su console i valori di 'name', 'role', 'profilePicture'
 for (let x = 0; x < arrayTeam.length; x++) {
     console.log(arrayTeam[x]['name']);
-    console.log(arrayTeam[x].role); // equivale a console.log(arrayTeam[x]['role']); sintassi che NON si può usare quando la CHIAVE contiene SPAZI
+    console.log(arrayTeam[x].role); // equivale a console.log(arrayTeam[x]['role']); sintassi che NON si può usare quando il nome della CHIAVE contiene SPAZI
     console.log(arrayTeam[x]['profilePicture']);
-    console.log(''); // creo spazio per rendere risultati più leggibili nella console
+    console.log(''); // creo spazio per rendere distinguibile, in console, ogni ripetizione del ciclo
 }
 
 // stampo su html i valori di ogni singola chiave, creando un nuovo <div> e riempiendolo ad ogni giro del ciclo
@@ -52,7 +52,7 @@ for (let y = 0; y < arrayTeam.length; y++) {
     const newDiv = document.createElement('div'); // creo <div>
     const containerDom = document.getElementById('container'); // richiamo il container
     containerDom.append(newDiv); // inserisco il <div> nel container
-    newDiv.innerHTML = (y+1) + ' --- '; // inserisco uno alla volta i vari valori
+    newDiv.innerHTML = (y+1) + ' --- '; // inserisco uno alla volta i singoli valori delle chiavi
     newDiv.innerHTML += arrayTeam[y]['name'] + ' --- ';
     newDiv.innerHTML += arrayTeam[y].role + ' --- ';
     newDiv.innerHTML += arrayTeam[y]['profilePicture'];

1a3d5bd922d4882a5873e328d70f346bc4e6565c
Luca Somaschini <somas99@gmail.com>
2022-07-18 18:54:27 +0200

    Bonus_2: improved js

--- Bonus_2/js/main.js
+++ Bonus_2/js/main.js
@@ -39,29 +39,24 @@ const arrayTeam = [
 
 ]
 
-// stampo su html i valori di ogni singola chiave, creando un nuovo <div> e riempiendolo ad ogni giro del ciclo
+// tramite un ciclo inserisco nel DOM un <div .card> che contiene: l'img, il nome, il ruolo
 for (let y = 0; y < arrayTeam.length; y++) {
     
+    // --- fase 1
     const containerDom = document.getElementById('container'); // richiamo il container
     const newDivCard = document.createElement('div'); // creo un nuovo <div> (sarà la card che contiene l'img e il testo)
-    newDivCard.classList.add('card'); // aggiungo la classe al <div> appena creato
-    containerDom.append(newDivCard); // aggiungo il <div> card al container
+    newDivCard.classList.add('card'); // aggiungo la classe .card al <div> appena creato
+    containerDom.append(newDivCard); // aggiungo il <div .card> al container
+    newDivCard.innerHTML = `<img src='${arrayTeam[y]['profilePicture']}' alt=Profile_pic />`; // inserisco l'img in .card
 
-    newDivCard.innerHTML = `<img src='${arrayTeam[y]['profilePicture']}' alt=Profile_pic />`; // inserisco l'img in card
+    // --- fase 2
+    const newDivName = document.createElement('p'); // creo nuovo tag <p>
+    newDivCard.append(newDivName); // inserisco tag <p> dentro .card (finisce sotto l'img perché l'img prende il 100% della larghezza - vedi css)
+    newDivName.innerHTML += arrayTeam[y]['name']; // inserisco in <p> il nome preso dall'oggetto nell'array (in base al ciclo prende il valore corrispondente)
 
-    const newDivName = document.createElement('p');
-    newDivCard.append(newDivName);
-    newDivName.innerHTML += arrayTeam[y]['name'];
+    // --- fase 3
+    const newDivRole = document.createElement('p'); // creo un secondo tag <p>
+    newDivCard.append(newDivRole); // inserisco il secondo tag <p> dentro .card (come sopra)
+    newDivRole.innerHTML += arrayTeam[y].role; // inserisco nel secondo <p> il ruolo preso dall'oggetto nell'array (in base al ciclo prende il valore corrispondente)
 
-    const newDivRole = document.createElement('p');
-    newDivCard.append(newDivRole);
-    newDivRole.innerHTML += arrayTeam[y].role;
-
-
-    /* const newDiv = document.createElement('div'); // creo <div>
-    containerDom.append(newDiv); // inserisco il <div> nel container
-    newDiv.innerHTML = (y+1) + ' --- '; // inserisco uno alla volta i singoli valori delle chiavi
-    newDiv.innerHTML += arrayTeam[y]['name'] + ' --- ';
-    newDiv.innerHTML += arrayTeam[y].role + ' --- ';
-    newDiv.innerHTML += `<img src='${arrayTeam[y]['profilePicture']}' alt=Profile_pic />`; // oppure posso mettere direttamente nella chiave 'profilePic' l'indirizzo ad ogni singola immagine dentro al tag <img> */
 }
